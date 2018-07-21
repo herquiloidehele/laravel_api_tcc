dfa641de4f5c7c17f2e0be16cdab16103e550a11
Herquiloide Raimundo Hele <herquiloide@gmail.com>
2018-07-21 11:26:19 +0200

    Salvando produto

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Produto;
+use App\Models\Variedade;
 use Illuminate\Http\Request;
+use Mockery\Exception;
 
 class ProdutoController extends ModelController
 {
@@ -17,6 +19,43 @@ class ProdutoController extends ModelController
 
 
 
+    public function store(Request $request)
+    {
+        $produto_request =  $request->all('designacao', 'categoria', 'variedades');
+        \DB::beginTransaction();
+
+
+            $produto = Produto::create(
+                [
+                    'designacao' => $produto_request['designacao'],
+                    'categoria_produtos_id' => $produto_request['categoria']['id']
+                ]);
+
+            if(!$produto){
+                \DB::rollBack();
+                throw new Exception('Erro ao criar um produto', 402);
+            }
+            else{
+                foreach ($produto_request['variedades'] as $variedade){
+                    $varie = Variedade::create(
+                        [
+                            'designacao' => $variedade,
+                            'produtos_id' => $produto->id
+                        ]
+                    );
+
+
+                    if (!$varie){
+                        \DB::rollBack();
+                        throw new Exception('Erro ao criar variedades', 402);
+                    }
+                }
+            }
+
+        \DB::commit();
+            return ['produto' => $produto];
+
+    }
 
 
 }
